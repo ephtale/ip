8a63937ef55778583e2b90fdb80fedc3cae589fc
ephtale <haruaozaki@outlook.com>
2026-01-20 11:17:03 +0800

    Level 1 complete

--- src/main/java/Aoko.java
+++ src/main/java/Aoko.java
@@ -3,6 +3,18 @@ public class Aoko {
         System.out.println("____________________________________");
         System.out.println("Hello! I'm Aoko, your Magecraft assistant.\nHow may I help you today?\n");
         System.out.println("____________________________________");
+
+        while(true) { 
+            String userInput = System.console().readLine();
+            if (userInput.toLowerCase().equals("bye")) {
+                break;
+            }
+            System.out.println("____________________________________");
+            System.out.println(userInput + "\n");
+            System.out.println("____________________________________");
+        }
+
+        System.out.println("____________________________________");
         System.out.println("See you again soon!\n");
         System.out.println("____________________________________");
     }
